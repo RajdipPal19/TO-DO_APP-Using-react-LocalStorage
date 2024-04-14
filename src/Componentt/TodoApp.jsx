afb35d8975600eb649d30afb242ef296e87c01f8
RAJDIP PAL <112768747+RajdipPal19@users.noreply.github.com>
2024-04-14 16:43:28 +0530

    third

--- src/Componentt/TodoApp.jsx
+++ src/Componentt/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import './TodoApp.css';
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
@@ -71,8 +71,8 @@ const TodoApp = () => {
 
   return (
     <div className="container mt-5">
-      <div className="row">
-        <div className="col">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
           <div className="input-group mb-3">
             <input
               type="text"
@@ -81,27 +81,26 @@ const TodoApp = () => {
               value={inputValue}
               onChange={handleInputChange}
             />
+            <button className="btn btn-primary my-custom-margin" type="button" onClick={handleAddTodo}>Add Todo</button>
+
           </div>
-        </div>
-        <div className="col-auto">
-          <button className="btn btn-primary" type="button" onClick={handleAddTodo}>Add Todo</button>
+          <ul className="list-group">
+            {todos.map((todo, index) => (
+              <li
+                key={index}
+                onClick={() => openModal(index)}
+                className={`list-group-item ${todo.completed ? 'bg-success text-white' : ''}`}
+                style={{ cursor: 'pointer' }}
+              >
+                {todo.text}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-      <ul className="list-group">
-        {todos.map((todo, index) => (
-          <li
-            key={index}
-            onClick={() => openModal(index)}
-            className={`list-group-item ${todo.completed ? 'bg-success text-white' : ''}`}
-            style={{ cursor: 'pointer' }}
-          >
-            {todo.text}
-          </li>
-        ))}
-      </ul>
       {modalOpen && (
         <div className="modal fade show" style={{ display: 'block' }}>
-          <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Edit Todo</h5>
@@ -113,8 +112,7 @@ const TodoApp = () => {
                   className="form-control mb-3"
                   value={modalTodoName}
                   onChange={(e) => setModalTodoName(e.target.value)}
-                />  
-                
+                />
                 <textarea
                   className="form-control mb-3"
                   value={modalTodoDescription}
@@ -128,7 +126,9 @@ const TodoApp = () => {
                 <button type="button" className="btn btn-danger" onClick={handleDeleteTodo}>
                   Delete
                 </button>
-                
+                {/* <button type="button" className="btn btn-secondary" onClick={closeModal}>
+                  Close
+                </button> */}
                 <button type="button" className="btn btn-success" onClick={handleToggleComplete}>
                   {todos[modalTodoIndex]?.completed ? 'Mark as Uncompleted' : 'Mark as Completed'}
                 </button>
